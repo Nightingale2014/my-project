cb541866f68bed39851b30c28ce970ee35c881f7
seoung342 <seoung342@gmail.com>
2025-04-21 19:50:44 +0900

    업데이트 내용

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,10 @@
-import { Link } from "react-router-dom"; // 이건 원래 있던 거 유지
+import { Link } from "react-router-dom";
 
 function Header() {
     return (
         <header className="w-full flex justify-between items-center px-8 py-4 bg-white shadow-md">
-            <div className="text-2xl font-bold">
-                <Link to="/">InterTicket</Link>
-            </div>
-
-            {/* 여기다 넣어! */}
-            <h1>Hello GitHub!</h1>
+            <div className="text-2xl font-bold"></div>
+            {/* 왼쪽 (비워둠) */}
 
             <div className="flex space-x-4">
                 <Link to="/login" className="px-4 py-2 text-sm font-medium text-gray-700 hover:text-blue-500">
@@ -19,6 +15,7 @@ function Header() {
                 </Link>
             </div>
         </header>
+
     );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Header from "../components/Header.jsx";
 
 function Home() {
     return (
-        <div className="relative min-h-screen"> {/* 부모 relative */}
+        <div className=" min-h-screen"> {/* 부모 relative */}
 
         <Header/>
 
